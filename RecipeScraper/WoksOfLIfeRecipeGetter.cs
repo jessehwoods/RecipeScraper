d52117e9a9df15f595471c078696e59bad89a65f
jessehwoods <jessehwoods@gmail.com>
2022-10-13 10:07:17 -0400

    Added xml summary and static keyword.

--- RecipeScraper/WoksOfLIfeRecipeGetter.cs
+++ RecipeScraper/WoksOfLIfeRecipeGetter.cs
@@ -1,7 +1,13 @@
 ﻿namespace RecipeScraper
 {
-    public class WoksOfLIfeRecipeGetter
+    /// <summary>
+    /// This class will  be used to access a recipe on <see href = "https://thewoksoflife.com/">Woks Of Life</see> by the url and return just the recipe as html.
+    /// </summary>
+    /// <remarks>
+    /// Because different websites have different html standards, this will be specific to <see href = "https://thewoksoflife.com/">Woks Of Life</see>. Other websites will require their own classes.
+    /// </remarks>
+    public static class WoksOfLIfeRecipeGetter
     {
-
+        
     }
 }
